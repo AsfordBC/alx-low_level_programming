74622201a3ac081dd220e5ffce4fea22ae5f31da
Asford Chilunga <asfordchilunga@gmail.com>
2022-11-01 03:53:42 -0700

    Write a function that fills memory with a constant byt

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,11 +2,11 @@
 
 /**
  * _memset - fill a block of memory with a specific value
- * @s: starting adress of memory to be filled
+ * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
  *
- * Return: changed array with new value foor n bytes
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
